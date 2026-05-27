41f92a091fa6808a7c651d716e13d35139d6066c
Bruno Zanon <bruno.zanon@ws-solution.com>
2026-05-26 22:48:58 -0300

    atualizando a cor da blade

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -59,7 +59,7 @@
 
                 <div class="mb-3 form-check">
                     <input type="checkbox" class="form-check-input" id="remember" name="remember">
-                    <label class="form-check-label" for="remember">Lembrar-me</label>
+                    <label class="form-check-label text-white" for="remember">Lembrar-me</label>
                 </div>
 
                 <div class="d-grid">
